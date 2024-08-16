fe3980a28b54fa22bf434031b20449aac8933aee
Red <reddd@inventati.org>
2024-08-16 16:46:54 -0400

    Something more

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 (function(window, document, undefined) {
-var max = 30;
-var random_number = Math.random() * max | 0;
-var front_vid_src = "webm/" + random_number.toString() + ".webm";
+var max = 49;
+function random_num()
+{
+	return Math.random() * max | 0;
+}
 
-// # create video element to append into above <a> tag
+var front_vid_src = "webm/" + random_num().toString() + ".webm";
+
 tmpElement = document.createElement("video");
 tmpElement.setAttribute("autoplay", "true");
 tmpElement.setAttribute("loop", "true");
 tmpElement.setAttribute("id", "front-vid-content");
+//tmpElement.setAttribute("controls", "true");
 tmpElement.setAttribute("src", front_vid_src);
 
 window.onload = init;
@@ -16,5 +20,9 @@ function init()
 {
 	var container = document.getElementById("front-vid");
 	container.appendChild(tmpElement);
+
+	document.getElementById("front-vid").addEventListener('click', function() {
+		document.getElementById("front-vid-content").src = "webm/" + random_num().toString() + ".webm";
+	});
 }
 })(window, document, undefined);
